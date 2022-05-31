2f33a2f7c82fb4620da4ca3c9f1e45ab1054e805
vVolodya <skinner.vova@gmail.com>
2022-05-31 18:57:01 +1000

    Added some pretty shitty UI

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let computerScore = 0;
-let userScore = 0;
-
 function computerPlay() {
 	let choices = ['Rock', 'Paper', 'Scissors'];
 	let randChoice = Math.floor(Math.random() * choices.length);
@@ -9,65 +6,75 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
-	playerSelection = playerSelection.toLowerCase();
-	computerSelection = computerSelection.toLowerCase();
-
-	if (playerSelection !== 'rock' && playerSelection !== 'paper' && playerSelection !== 'scissors') {
-		alert('Can you even play?! Please, choose another fighter!');
-	}
-	
 	if (playerSelection === computerSelection) {
-		alert('Draw!');
+		gameResultPara.textContent = 'Draw!';
 	}
+	if ((playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+		 (playerSelection === 'Rock'&& computerSelection === 'Scissors') || 
+		 (playerSelection === 'Paper' && computerSelection === 'Rock')) {
+		gameResultPara.textContent = 'You win!';
+		userScore++;
+	}
+	if ((playerSelection === 'Rock'&& computerSelection === 'Paper') ||
+		 (playerSelection === 'Paper' && computerSelection === 'Scissors') ||
+		 (playerSelection === 'Scissors' && computerSelection === 'Rock')) {
+		gameResultPara.textContent = 'You lost!';
+		computerScore++;
+	}
+}
 
-	if (playerSelection === 'scissors' && computerSelection === 'paper') {
-		userScore++;
-		alert('You win! Scissors beats paper!');
-	}
-	if (playerSelection === 'rock' && computerSelection === 'scissors') {
-		userScore++;
-		alert('You win! Rock beats scissors!');
-	}
-	if (playerSelection === 'paper' && computerSelection === 'rock') {
-		userScore++;
-		alert('You win! Paper beats Rock!');
-	}
+let computerScore = 0;
+let userScore = 0;
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+const buttonContainer = document.querySelector('.btn-container');
+const gameResultPara = document.querySelector('.gameResults');
+const userScorePara = document.querySelector('.userScore');
+const computerScorePara = document.querySelector('.computerScore');
+const endGamePara = document.querySelector('.endGame');
+const restartButton = document.querySelector('.restart-btn');
 
-	if (playerSelection === 'rock' && computerSelection === 'paper') {
-		computerScore++;
-		alert('You lose! Paper beats rock!');
-	}
-	if (playerSelection === 'paper' && computerSelection === 'scissors') {
-		computerScore++;
-		alert('You lose! Scissors beats Paper!');
-	}
-	if (playerSelection === 'scissors' && computerSelection === 'rock') {
-		computerScore++;
-		alert('You lose! Rock beats Scissors!');
+function handleClick(playerSelection) {
+	const computerSelection = computerPlay();
+	playRound(playerSelection, computerSelection);
+	userScorePara.textContent = `User score: ${userScore}`;
+	computerScorePara.textContent = `Computer score: ${computerScore}`;
+	endGameResult();
+}
+
+function endGameResult() {
+	if(computerScore === 3 || userScore == 3) {
+		gameResultPara.textContent = '';
+		buttonContainer.classList.add('removeBtns');
+		restartButton.classList.add('active');
+		return endGame();
 	}
 }
 
 function endGame() {
-	if (userScore === computerScore) {
-		return alert('Booooring!')
-	}
-	if (userScore > computerScore) {
-		return alert('Winner! Winner! Chicken Dinner!')
-	}
-	if (userScore < computerScore) {
-		return alert('Looooser! Go cryyyy!')
-	}
+	return userScore > computerScore
+	? (endGamePara.textContent = 'Winner! Winner! Chicken Dinner!')
+	: (endGamePara.textContent = 'Looooser! Go cryyyy!');
 }
 
-function play() {
-	while((computerScore < 3) && (userScore < 3)) {
-		let computerSelection = computerPlay();
-		let playerSelection = prompt('Choose: rock, paper, scissors. Until 3 wins.', computerPlay());
-		playRound(playerSelection, computerSelection);
-		alert(`Your score is ${userScore}. Computer score is ${computerScore}`);
-	}
-	endGame()
+function restartGame() {
+	computerScore = 0;
+	userScore = 0;
+	gameResultPara.textContent = 'Choose your weapon!';
+	userScorePara.textContent = 'User score: 0';
+	computerScorePara.textContent = 'Computer score: 0';
+	endGamePara.textContent = '';
+	restartButton.classList.remove('active');
+	buttonContainer.classList.remove('removeBtns');
 }
 
-play()
+const handleCLickRock = () => handleClick('Rock');
+const handleCLickPaper = () => handleClick('Paper');
+const handleCLickScissors = () => handleClick('Scissors');
+
+rock.addEventListener('click', handleCLickRock);
+paper.addEventListener('click', handleCLickPaper);
+scissors.addEventListener('click', handleCLickScissors);
+restartButton.addEventListener('click', restartGame);
+
